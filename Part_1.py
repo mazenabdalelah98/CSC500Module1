29a01981dd1003bca3c3079f4dd4269c9a31474c
mazenabdalelah98 <mazen.abdalelah@csuglobal.edu>
2025-11-16 14:30:58 +0300

    Add files via upload

---
+++ Part_1.py
@@ -0,0 +1,10 @@
+# Part 1: Addition and Subtraction
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+
+print("Addition result:", addition)
+print("Subtraction result:", subtraction)
