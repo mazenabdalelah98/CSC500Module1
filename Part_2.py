613af508e90db9d300174bfdb6f44d3cc38c1ead
mazenabdalelah98 <mazen.abdalelah@csuglobal.edu>
2025-11-16 14:32:29 +0300

    Add files via upload

---
+++ Part_2.py
@@ -0,0 +1,10 @@
+# Part 2: Multiplication and Division
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Multiplication result:", multiplication)
+print("Division result:", division)
